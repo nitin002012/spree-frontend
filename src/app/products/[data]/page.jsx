10c8ddb449d0f465bcbac0ad9932ac7ba7e5ce1b
nitin002012 <nitin.kabi@hyscaler.com>
2024-06-13 18:20:28 +0530

    dynamic routing

---
+++ src/app/products/[data]/page.jsx
@@ -0,0 +1,38 @@
+'use client'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ProductCard from "../productCard";
+
+const ProductItem = ({ params }) => {
+    console.log(params);
+    const [products, setProducts] = useState([]);
+    const [addToCart, setAddToCart] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/api/v1/products");
+                console.log(response.data);
+                setProducts(response.data);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const handleClick = (item) => {
+        setAddToCart([...addToCart, item]);
+        alert(`Added to cart successfully`);
+    };
+
+    return (
+        <>
+            <h1 className="p-2">This is a product page of category of {params.data}</h1>
+            {products.filter((item) => item.meta_keywords === params.data).map((item) => (
+                <ProductCard key={item.id} handleClick={handleClick} product={item} />
+            ))}
+        </>
+    );
+};
+export default ProductItem;
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,8 +2,15 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import ProductCard from "./productCard";
+import { redirect, useRouter } from "next/navigation";
+
 
 const ProductPage = () => {
+  const data = [{name:"t-shirts", route1 :"/t-shirts"},
+              {name:"formal-wear", route1 :"/formal-wear"},
+              {name:'trousers', route1 :"/trousers"}]
+
+  const router = useRouter();
   const [products, setProducts] = useState([]);
   const [addToCart, setAddToCart] = useState([]);
 
@@ -21,23 +28,49 @@ const ProductPage = () => {
     fetchData();
   }, []);
 
-  console.log("products are ", products);
+  const extractDistinctMetaKeywords = (data) => {
+    const metaKeywordsSet = new Set();
+    data.forEach(item => {
+      if (item.meta_keywords) {
+        metaKeywordsSet.add(item.meta_keywords);
+      }
+    });
+    return Array.from(metaKeywordsSet);
+  };
+  
+  const distinctMetaKeywords = extractDistinctMetaKeywords(products);
+  
+  console.log("meta keywords are", distinctMetaKeywords);
+
+  const categorizedProducts = {};
+
+  distinctMetaKeywords.forEach(keyword => {
+    categorizedProducts[keyword] = products.filter(product => product.meta_keywords === keyword);
+  });
 
   const handleClick = (item) => {
-    setAddToCart([...addToCart, item]); 
+    setAddToCart([...addToCart, item]);
     alert(`Added to cart successfully`);
   };
 
-  console.log(addToCart);
-
   return (
     <>
-<h1 className="text-4xl p-5 font-mono text-center underline ">Products</h1>
-      <div className="flex flex-wrap justify-center items-center gap-5">
-        {products.map((item) => (
-          <ProductCard key={item.id} product={item} handleClick={handleClick} />
-        ))}
-      </div>
+      <h1 className="text-4xl p-5 font-mono text-center underline">Products</h1>
+
+      {Object.entries(categorizedProducts).map(([keyword, products]) => (
+        <div key={keyword}>
+          <p className="text-3xl font-bold w-8/12 mx-auto">{keyword.toLocaleUpperCase()}</p>
+          <div className="flex flex-wrap justify-center items-center gap-5 my-7">
+            {products.map(item => (
+              <div className=" ">
+                <ProductCard key={item.id} handleClick={handleClick} product={item} />
+
+                <hr/>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </>
   );
 };
